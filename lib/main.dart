d93fd41d763c5d9caf537712e9d486bdba6cff38
Raff Evald <rafael-ev-silva@outlook.com>
2022-06-28 22:26:31 -0300

    Criando a PAGINA DE LOGIN e a HOME PAGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pay_flow/modules/home/home_page.dart';
+import 'package:pay_flow/modules/login/login_page.dart';
 import 'package:pay_flow/shared/themes/appcolors.dart';
 import 'modules/splash/splash_page.dart';
 
@@ -12,11 +14,11 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PayFlow',
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
