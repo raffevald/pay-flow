7e0a75a6f0fe7e08e807e4bee8e4afcc0da6f094
Raff Evald <rafael-ev-silva@outlook.com>
2022-07-07 21:03:30 -0300

    Retirando coisas desnecessarias

--- lib/app_widger.dart
+++ lib/app_widger.dart
@@ -1,22 +1,13 @@
-// import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:pay_flow/modules/home/home_page.dart';
 import 'package:pay_flow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:pay_flow/modules/splash/splash_page.dart';
 import 'package:pay_flow/shared/models/user_model.dart';
-// import 'modules/barcode_scanner/barcode_scanner_page.dart';
-// import 'modules/barcode_scanner/barcode_scanner_page.dart';
-import 'modules/barcode_scanner/teste.dart';
 import 'modules/login/login_page.dart';
-// import 'shared/models/user_model.dart';
 import 'shared/themes/appcolors.dart';
 
 class AppWidget extends StatelessWidget {
-  // const AppWidget({Key? key}) : super(key: key);
-
   AppWidget({Key? key}) : super(key: key) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown,
@@ -41,10 +32,8 @@ class AppWidget extends StatelessWidget {
         "/home": ((context) => HomePage(
               user: ModalRoute.of(context)!.settings.arguments as UserModel,
             )),
-        // "/home": (context) => HomePage(
-        //     user: ModalRoute.of(context)!.settings.arguments as UserModel),
         "/login": ((context) => LoginPage()),
-        "/barcode_scanner": (context) => BarcodeScannerPage(),
+        // "/barcode_scanner": (context) => BarcodeScannerPage(),
         "/insertBoletos": ((context) => InserirBoletosPage()),
       },
     );
